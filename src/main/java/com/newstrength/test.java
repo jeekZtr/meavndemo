8a229a142de8a7f4554387fb0dd95f7d93d45b80
zhangtairan <894615495@qq.com>
2018-09-14 11:19:58 +0800

    测试提交23

--- src/main/java/com/newstrength/test.java
+++ src/main/java/com/newstrength/test.java
@@ -5,15 +5,6 @@ public class test {
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
 
     }
 }
